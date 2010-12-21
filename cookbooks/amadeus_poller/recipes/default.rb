e9e3b7de33e6f6f622204a79cde531070d97c6f3
Ryan Oberholzer <ryan@platform45.com>
2010-12-21 14:33:01 +0200

    Amadeus API loader

--- cookbooks/amadeus_poller/recipes/default.rb
+++ cookbooks/amadeus_poller/recipes/default.rb
@@ -2,3 +2,25 @@
 # Cookbook Name:: amadeus_poller
 # Recipe:: default
 #
+
+ey_cloud_report "AmadeusPoller" do
+  message "configuring amadeus poller"
+end
+
+if node[:instance_role] == "solo"
+  template "/etc/monit.d/amadeus_poller.monitrc" do
+    source "amadeus.monitrc.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name]
+    })
+  end
+  
+  execute "monit-reload-restart" do
+     command "sleep 30 && monit quit"
+     action :run
+  end
+end
