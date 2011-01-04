8ab7a62edecf6ca72df44792a042868aad5f7184
Ryan Oberholzer <ryan@platform45.com>
2011-01-04 15:03:16 +0200

    Completed amadeus email parser

--- cookbooks/amadeus_poller/recipes/default.rb
+++ cookbooks/amadeus_poller/recipes/default.rb
@@ -6,21 +6,22 @@
 ey_cloud_report "AmadeusPoller" do
   message "configuring amadeus poller"
 end
-
-if node[:instance_role] == "solo"
-  template "/etc/monit.d/amadeus_poller.monitrc" do
-    source "amadeus.monitrc.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    variables({
-      :app_name => app_name,
-      :user => node[:owner_name]
-    })
-  end
+node[:applications].each do |app_name,data|
+  if node[:instance_role] == "solo"
+    template "/etc/monit.d/amadeus_poller.monitrc" do
+      source "amadeus.monitrc.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables({
+        :app_name => "",
+        :user => node[:owner_name]
+      })
+    end
   
-  execute "monit-reload-restart" do
-     command "sleep 30 && monit quit"
-     action :run
+    execute "monit-reload-restart" do
+       command "sleep 30 && monit quit"
+       action :run
+    end
   end
 end
